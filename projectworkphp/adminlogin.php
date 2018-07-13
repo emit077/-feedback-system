2074f85b74ec6d0a0240d99a2fc236f878d72239
emit077 <30339332+emit077@users.noreply.github.com>
2018-07-13 21:30:28 +0530

    Add files via upload

---
+++ projectworkphp/adminlogin.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="adminlogin.css">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<style type="text/css">
+		@import url(https://fonts.googleapis.com/css?family=Raleway:400,200);
+@import url(https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css);
+*
+	</style>
+</head>
+<body>
+<div id="headcontainer">
+		<h1>FEEDBACK SYSTEM</h1>
+	</div>
+	  <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+   <a href="#news" style="margin-left: 1000px">Login</a>
+</div>
+
+  <div class="overlay">
+</div>
+<div class="leftcontent">
+  <span class="ltitle">Access More</span>
+  <br>
+  <span class="lcontent">
+    With Citus, you can unleash your desktop, and let the powers of desktop widgets come to life! Even though Citus is free, we still need to store user-specific information, like which widgets you have installed, and where they are positioned. By logging in, we can save that information to your account, and allow you to have a better experience overall.<br><br>Just login on the right to access your dashboard where you can add, edit, and remove widgets!
+  </span>
+</div>
+<div class="logincont">
+  <span class="rtitle">Login</span>
+  <br>
+  <br>
+  <form>
+    <input class="textinput" type="text" name="username" autofocus="autofocus" placeholder="Username">
+    <br>
+    <br>
+    <input class="textinput" type="password" name="password" placeholder="Password">
+    <br>
+    <br>
+    <button class="submit" type="submit">
+      <img src="http://www.yachtsale.se/wp-content/themes/Yachtsales/images/arrow-right.png" height="24px" />
+    </button>
+  </form>
+</div></body>
+<html>
---
+++ projectworkphp/demo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		#con{
+			background-color: hotpink;
+			width: 100%;
+			height: 500px;
+		}
+	</style>
+</head>
+<body>
+<div id="con"></div>
+</body>
+</html>
